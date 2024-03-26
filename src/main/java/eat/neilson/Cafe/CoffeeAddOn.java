6d9052a56b7099a474b6b10f9ee2f146b3ad7964
sbphantom <62124940+sbphantom@users.noreply.github.com>
2024-03-26 17:16:31 -0400

    updated toString method

--- src/main/java/eat/neilson/Cafe/CoffeeAddOn.java
+++ src/main/java/eat/neilson/Cafe/CoffeeAddOn.java
@@ -16,7 +16,15 @@ public enum CoffeeAddOn {
 
     @Override
     public String toString() {
-        return this.name().charAt(0) + this.name().substring(1).toLowerCase();
+        String[] words = this.name().split("_");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++){
+            if (i != 0) sb.append(" ");
+            sb.append(words[i].charAt(0)).append(words[i].substring(1).toLowerCase());
+        }
+
+        return sb.toString();
     }
 
 }
--- src/main/java/eat/neilson/Cafe/DonutFlavor.java
+++ src/main/java/eat/neilson/Cafe/DonutFlavor.java
@@ -28,6 +28,14 @@ public enum DonutFlavor {
 
     @Override
     public String toString() {
-        return this.name().charAt(0) + this.name().substring(1).toLowerCase();
+        String[] words = this.name().split("_");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++){
+            if (i != 0) sb.append(" ");
+            sb.append(words[i].charAt(0)).append(words[i].substring(1).toLowerCase());
+        }
+
+        return sb.toString();
     }
 }
