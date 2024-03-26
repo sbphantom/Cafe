58cafde4c45bc53716cfc9006a8fadbf01d1101d
sbphantom <dannyonuorah@gmail.com>
2024-03-26 13:31:02 -0400

    created abstract methods

--- src/main/java/eat/neilson/Cafe/MenuItem.java
+++ src/main/java/eat/neilson/Cafe/MenuItem.java
@@ -7,4 +7,20 @@ public abstract class MenuItem {
 
 
     public abstract double price();
+
+    @Override
+    public abstract String toString();
+
+    //@Override
+    public abstract int compareTo(MenuItem item);
+
+    @Override
+    public abstract boolean equals(Object obj);
+
+    @Override
+    public abstract int hashCode();
+
+
+
+
 }
