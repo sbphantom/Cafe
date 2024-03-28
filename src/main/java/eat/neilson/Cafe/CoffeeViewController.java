f5cd92f8695fe3c1f2049f26167e4b8e8fba9d8a
sbphantom <62124940+sbphantom@users.noreply.github.com>
2024-03-28 12:30:01 -0400

    implemented add order button

--- src/main/java/eat/neilson/Cafe/CoffeeViewController.java
+++ src/main/java/eat/neilson/Cafe/CoffeeViewController.java
@@ -1,12 +1,17 @@
 package eat.neilson.Cafe;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 
 public class CoffeeViewController {
@@ -14,7 +19,7 @@ public class CoffeeViewController {
     @FXML
     public ImageView coffeeImage;
     @FXML
-    public Spinner<Integer> coffeeQuanitySpinner;
+    public Spinner<Integer> coffeeQuantitySpinner;
     public TextField coffeeSubtotalTextField;
     public Button addOrderButton;
     public Button cancelOrderButton;
@@ -26,35 +31,39 @@ public class CoffeeViewController {
     @FXML
 
     public ColumnConstraints coffeeAddOnColumn;
+
+    public ArrayList<CheckBox> coffeeAddOnOptions = new ArrayList<>();
+
     ToggleGroup coffeeSizeToggleGroup = new ToggleGroup();
 
     private CafeViewController app;
-    private Stage stage;
-    private Scene primaryScene;
-    private Stage primaryStage;
+//    private Stage stage;
+//    private Scene primaryScene;
+//    private Stage primaryStage;
 
-    private Coffee coffee = new Coffee();
+    private Coffee coffee;
 
     public void initialize() {
-        addRadioButtonsToCoffeeSizeColumn();
-        addCheckboxesToCoffeeAddOnColumn();
-        coffeeQuanitySpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 5));
-        coffeeQuanitySpinner.getValueFactory().setValue(1);
-        coffeeQuanitySpinner.valueProperty().addListener((obs, oldValue, newValue) -> {
-            updateSubtotal();
+        addCoffeeSizeButtons();
+        addCoffeeAddOnBoxes();
+        coffeeQuantitySpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 5));
+        coffeeQuantitySpinner.getValueFactory().setValue(1);
+        coffeeQuantitySpinner.valueProperty().addListener((obs, oldValue, newValue) -> {
+            if (coffee != null) updateSubtotal();
         });
 
-        coffee.setCoffeeSize((CoffeeSize) coffeeSizeToggleGroup.getSelectedToggle().getUserData());
+//        coffee.setCoffeeSize((CoffeeSize) coffeeSizeToggleGroup.getSelectedToggle().getUserData());
+        coffee = new Coffee((CoffeeSize) coffeeSizeToggleGroup.getSelectedToggle().getUserData(), new ArrayList<>());
         updateSubtotal();
     }
 
 
     private void updateSubtotal(){
-        double subtotal = coffee.price() * coffeeQuanitySpinner.getValue();
+        double subtotal = coffee.price() * coffeeQuantitySpinner.getValue();
         String formattedSubtotal = String.format("%.2f", subtotal);
         coffeeSubtotalTextField.setText("$" + formattedSubtotal);
     }
-    private void addRadioButtonsToCoffeeSizeColumn() {
+    private void addCoffeeSizeButtons() {
         int row = 1;
         for (CoffeeSize size : CoffeeSize.values()) {
             RadioButton radioButton = new RadioButton(size.toString());
@@ -79,14 +88,14 @@ public class CoffeeViewController {
         }
 
         coffeeSizeToggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
+            if (newValue != null && coffee != null) {
                 coffee.setCoffeeSize((CoffeeSize) coffeeSizeToggleGroup.getSelectedToggle().getUserData());
                 updateSubtotal();
             }
         });
     }
 
-    private void addCheckboxesToCoffeeAddOnColumn() {
+    private void addCoffeeAddOnBoxes() {
         int row = 1;
         for (CoffeeAddOn addOn : CoffeeAddOn.values()) {
             CheckBox checkBox = new CheckBox(addOn.toString());
@@ -99,7 +108,7 @@ public class CoffeeViewController {
                 }
                 updateSubtotal();
             });
-
+            coffeeAddOnOptions.add(checkBox);
             coffeeGridPane.add(checkBox, 1, row);
 
             if (coffeeGridPane.getRowConstraints().size() <= row) {
@@ -113,19 +122,43 @@ public class CoffeeViewController {
         }
     }
 
-    public void setMainController(CafeViewController controller,
-                                  Stage stage,
-                                  Stage primaryStage,
-                                  Scene primaryScene) {
+    public void setMainController(CafeViewController controller) {
         app = controller;
-        this.stage = stage;
-        this.primaryStage = primaryStage;
-        this.primaryScene = primaryScene;
     }
 
-    public void onAddOrderButtonClick(ActionEvent actionEvent) {
+//    public void setMainController(CafeViewController controller,
+//                                  Stage stage,
+//                                  Stage primaryStage,
+//                                  Scene primaryScene) {
+//        app = controller;
+//        this.stage = stage;
+//        this.primaryStage = primaryStage;
+//        this.primaryScene = primaryScene;
+//    }
+
+    public void onAddOrderButtonClick() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Add to Order");
+        alert.setContentText("Add " + coffeeQuantitySpinner.getValue()+ " coffee to order?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            app.addItemToOrder(coffee, coffeeQuantitySpinner.getValue());
+
+            coffee = null;
+
+            coffeeQuantitySpinner.getValueFactory().setValue(1);
+            coffeeSizeToggleGroup.selectToggle(coffeeSizeToggleGroup.getToggles().getFirst());
+            for (CheckBox box : coffeeAddOnOptions){
+                box.setSelected(false);
+            }
+
+            coffee = new Coffee((CoffeeSize) coffeeSizeToggleGroup.getSelectedToggle().getUserData(), new ArrayList<>());
+            updateSubtotal();
+        }
     }
 
-    public void onCancelOrderButtonClick(ActionEvent actionEvent) {
+    public void onCancelOrderButtonClick() {
     }
 }
