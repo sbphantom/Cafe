db45fc79f6c0d698eafd71cffbc61d7cdfd78a56
sbphantom <62124940+sbphantom@users.noreply.github.com>
2024-03-28 12:27:55 -0400

    addItem()

--- src/main/java/eat/neilson/Cafe/CafeMain.java
+++ src/main/java/eat/neilson/Cafe/CafeMain.java
@@ -7,14 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class CafeMain extends Application {
-
     ArrayList<Order> orderHistory = new ArrayList<>();
-
-    Order currentOrder = null;
-
-
+    Order currentOrder = new Order();
     public boolean createOrder() {
         if (currentOrder == null) {
             currentOrder = new Order();
@@ -25,22 +22,32 @@ public class CafeMain extends Application {
 
 
     public boolean addOrder() {
-        if (currentOrder != null) {
+        if (currentOrder.cartSize() > 0) {
             orderHistory.add(currentOrder);
-            currentOrder = null;
+            currentOrder = new Order();
             return true;
         }
         return false;
     }
 
+    public boolean addItem(MenuItem item, int quantity) {
+        currentOrder.addItem(item, quantity);
+
+        for (Map.Entry<MenuItem, Integer> entry : currentOrder.getCart().entrySet()){
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+        System.out.println("-----");
+
+        return true;
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CafeMain.class.getResource("cafe-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         CafeViewController cafeController = fxmlLoader.getController();
-        cafeController.setPrimaryStage(stage, scene);
-        stage.setTitle("Hello!");
+        cafeController.setPrimaryStage(stage);
+        stage.setTitle("Neilson Cafe");
         stage.setScene(scene);
         stage.show();
     }
