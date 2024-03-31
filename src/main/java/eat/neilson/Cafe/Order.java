177a3437db8fa3eb2201c09fff07aff4f71641f3
0fficialAsim <adeola.asim@gmail.com>
2024-03-30 23:15:16 -0400

    working on donut

--- src/main/java/eat/neilson/Cafe/Order.java
+++ src/main/java/eat/neilson/Cafe/Order.java
@@ -2,12 +2,13 @@ package eat.neilson.Cafe;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Order {
     private int orderNumber;
-    private double subtotal;
+    private double subtotal = 0.0;
 
-    private double total;
+    private double total = 0.0;
 
     private ArrayList<MenuItem> items = new ArrayList<>();
     private HashMap<MenuItem, Integer> cart = new HashMap<>();
@@ -15,6 +16,16 @@ public class Order {
     public static double STATE_TAX = 6.625 / 100;
 
 
+
+    public Order(int num){
+        this.orderNumber = num;
+    }
+
+
+
+    public Integer getOrderNumber(){
+        return orderNumber;
+    }
 
     public HashMap<MenuItem, Integer> getCart(){
         return cart;
@@ -53,32 +64,47 @@ public class Order {
         }
     }
 
+    public double getSubtotal(){
+        subtotal = 0;
+        for (Map.Entry<MenuItem, Integer> entry : cart.entrySet()){
+            subtotal += (entry.getKey().price() * entry.getValue());
+        }
+        return subtotal;
+    }
+
+    public double tax(){
+        return subtotal * STATE_TAX;
+    }
+
+    public double getTotal(){
+        return subtotal + tax();
+    }
+
     public static void main(String[] args) {
 
-        Order order = new Order();
-        Coffee a = new Coffee();
-        a.setCoffeeSize(CoffeeSize.GRANDE);
-        a.addAddOn(CoffeeAddOn.CARAMEL);
-        a.addAddOn(CoffeeAddOn.FRENCH_VANILLA);
-
-        Coffee b = new Coffee();
-        b.setCoffeeSize(CoffeeSize.GRANDE);
-        b.addAddOn(CoffeeAddOn.FRENCH_VANILLA);
-        b.addAddOn(CoffeeAddOn.CARAMEL);
-
-        //order.cart.put(a, 1);
-
-        System.out.println(order.cart.containsKey(a));
-        System.out.println(order.cart.containsKey(b));
-
-        order.addItem(a, 3);
-        order.addItem(b, 5);
-        order.removeItem(b, 2);
-        System.out.println(order.itemCount(b));
-
+//        Order order = new Order(0100);
+//        Coffee a = new Coffee();
+//        a.setCoffeeSize(CoffeeSize.GRANDE);
+//        a.addAddOn(CoffeeAddOn.CARAMEL);
+//        a.addAddOn(CoffeeAddOn.FRENCH_VANILLA);
+//
+//        Coffee b = new Coffee();
+//        b.setCoffeeSize(CoffeeSize.GRANDE);
+//        b.addAddOn(CoffeeAddOn.FRENCH_VANILLA);
+//        b.addAddOn(CoffeeAddOn.CARAMEL);
+//
+//        //order.cart.put(a, 1);
+//
+//        System.out.println(order.cart.containsKey(a));
+//        System.out.println(order.cart.containsKey(b));
+//
+//        order.addItem(a, 3);
+//        order.addItem(b, 5);
+//        order.removeItem(b, 2);
+//        System.out.println(order.itemCount(b));
+//
 
     }
 
 
 }
-
